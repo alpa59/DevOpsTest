2560e3a83c4cd5d7963c3ed6e7b0223c531e7fc6
Thomas-Frederiksen <thomas.frederiksen07@gmail.com>
2025-02-06 13:21:17 +0100

    Update app.py

--- Docker/app.py
+++ Docker/app.py
@@ -1,4 +1,4 @@
-import unittest
+
 from flask import Flask
 
 app = Flask(__name__)
@@ -9,16 +9,4 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
-
-def add(a, b):
-    return a + b
-
-def testadd():
-    assert 1 + 1 == 2
-
-class TestMathOperations(unittest.TestCase):
-    def test_add(self):
-        self.assertEqual(add(2, 3), 5)
-        self.assertEqual(add(-1, 1), 0)
-        self.assertEqual(add(0, 0), 0)
 
