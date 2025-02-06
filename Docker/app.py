f2f6859cab7d8bcb5c0d926a8cba4aa3027e65b0
Thomas-Frederiksen <thomas.frederiksen07@gmail.com>
2025-02-06 13:18:36 +0100

    best i test

--- Docker/app.py
+++ Docker/app.py
@@ -1,3 +1,4 @@
+<<<<<<< Updated upstream
 from flask import Flask
 
 app = Flask(__name__)
@@ -8,3 +9,21 @@ def home():
 
 if __name__ == "__main__":
     app.run(host="0.0.0.0", port=5000)
+=======
+print("ligma")
+
+import unittest
+
+def add(a, b):
+    return a + b
+
+def testadd():
+    assert 1 + 1 == 2
+
+class TestMathOperations(unittest.TestCase):
+    def test_add(self):
+        self.assertEqual(add(2, 3), 5)
+        self.assertEqual(add(-1, 1), 0)
+        self.assertEqual(add(0, 0), 0)
+
+>>>>>>> Stashed changes
