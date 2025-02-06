c44f5fde9a3682c5d799606c6a7cca80f4b11550
AAL <aal@lyngsoesystems.com>
2025-02-06 13:00:55 +0100

    Breaking changes

--- Docker/app.py
+++ Docker/app.py
@@ -1,1 +1,10 @@
-print("ligma")
+from flask import Flask
+
+app = Flask(__name__)
+
+@app.route("/")
+def home():
+    return "Hello, DevOps World! Your CI/CD pipeline is working!"
+
+if __name__ == "__main__":
+    app.run(host="0.0.0.0", port=5000)
