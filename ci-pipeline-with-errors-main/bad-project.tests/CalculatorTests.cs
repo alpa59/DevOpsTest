a228d2d47cf0f2cc4fcdcb30d8d35e2fe235db87
Thomas-Frederiksen <thomas.frederiksen07@gmail.com>
2025-02-17 13:23:56 +0100

    feature/bad-project-pipeline

---
+++ ci-pipeline-with-errors-main/bad-project.tests/CalculatorTests.cs
@@ -0,0 +1,11 @@
+﻿namespace bad_project.tests;
+
+public class CalculatorTests
+{
+    [Fact]
+    public void Add_ReturnsIncorrectResult()
+    {
+        var calc = new Calculator();
+        Assert.Equal(5, calc.Add(2, 2)); // Fejlen her: 2+2=4, ikke 5
+    }
+}
